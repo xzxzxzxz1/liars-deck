fb3340d2db7e51abd50537f3311ce423c822611e
Joshua Zhang <joshuazhang@Mac.lan>
2025-01-20 04:58:36 +1100

    Classes fully implemented for first tests

--- src/game_logic.py
+++ src/game_logic.py
@@ -1,4 +1,4 @@
-
+import random
 from player import Player
 
 class Guest:
@@ -34,14 +34,14 @@ class Lobby:
         if guest in self.guests:
             print("ERROR: Name already taken. Please try a new name")
             return False
-        self.players.append(guest)
+        self.guests.append(guest)
         print("{guest} has joined the lobby.".format(guest = guest.name))
     
     def remove_guest(self, guest):
         # Player removed by the lobby creator
         # Might need case where they can't remove themselves otherwise that will cause error
         if guest in self.guests:
-            self.players.remove(guest)
+            self.guests.remove(guest)
             print("{guest} has been removed from the lobby.".format(guest = guest.name))
         else:
             print("ERROR: Guest to remove doesn't exist")
@@ -76,43 +76,165 @@ class Game:
     def __init__(self, guests):
         self.players = [Player(guest) for guest in guests]
         self.num_players = len(self.players)
-        self.rounds = []
         self.ongoing = True
+        self.winner = None
         
-        def play_game(self):
-            while self.ongoing == True:
-                # Keeps track of the round and also continues initiating Round class as long 
-                # as game status is True / Ongoing
-                self.rounds.append(Round(self.players))
-                
-            # Once game status toggles to false we will close the game and 
-            # award victory to the last remaining player
-            return False
-            # Should exit game here?
-        
-        def exit_game(self):
-            # How do i even code this?
-        
+    def play_game(self):
+        while self.ongoing == True:
+            # Keeps track of the round and also continues initiating Round class as long 
+            # as game status is True / Ongoing
+            Round(self.players)
+            self.status_update
+        # Once game status toggles to false we will close the game and 
+        # award victory to the last remaining player
+        self.winner = self.players[0]
+        return self.winner
+    
+    def status_update(self):
+        self.players = [player for player in self.players if player.alive]
+        self.num_players = len(self.players)
+        if self.num_players == 1:
+            self.ongoing = False
+        return True
+
 class Round:
     def __init__(self, players):
-        pass
-        
-    def initialise_hands(self, players):
-        # Randomly distributes 5 cards to each of the players from a deck of
-        # 6 Kings, 6 Aces, 6 Queens, 2 Jokers
-        pass
-    
+        self.players = players
+        self.ongoing = True
+        self.playcard = self.initialise_playcard()
+        self.initialise_hands()
+        self.previous_hand = None
+        self.previous_player = None
+
     def play_round(self):
-        pass
+        while self.ongoing == True:
+            for player in self.players:
+                current_turn = Turn(player)
+                if current_turn == 'B':
+                    self.call_bluff(player, self.previous_player)
+                else: 
+                    amount = len(current_turn)
+                    print(f"{player}: {amount} {self.playcard}!")
+                    self.previous_hand = current_turn
+                    self.previous_player = player
+        return True
+        
+    def initialise_playcard(self):
+        draw = ['A', 'K', 'Q']
+        round_card = random.choice(draw)
+        if round_card == 'A':
+            round_card_print = 'Ace'
+        elif round_card == 'K':
+            round_card_print = 'King'
+        elif round_card == 'Q':
+            round_card_print = 'Queen'
+        
+        print(f"{round_card_print}'s Table!")
+        
+        return round_card
         
     def start_turn_timer(self):
         # 30 second timer 
         self.end_turn()
         pass
 
-    def end_turn(self):
-        pass
-    
+    def initialise_hands(self):
+        # Randomly distributes 5 cards to each of the players from a deck of
+        # 6 Kings, 6 Aces, 6 Queens, 2 Jokers
+        deck = ['A'] * 6 + ['K'] * 6 + ['Q'] * 6 + ['J'] * 2
+        random.shuffle(deck)
+        
+        for player in self.players:
+            player.hand = [deck.pop() for _ in range(5)]
 
+        print("There are 6 Aces, 6 Kings, 6 Queens and 2 Jokers in the deck.")
+    
+    def roulette(self, player):
+        bullet_chamber = random.randint(1, player.max_lives)
+        shot = random.randint(1, player.max_lives)
+        print("Waiting...")
+        # Make a 3 second wait timer
+        
+        if shot == bullet_chamber:
+            print("The chamber contained a bullet!")
+            player.eliminate()
+        else:
+            print("The chamber was empty! " + player.name + " lives another day!")
+            player.max_lives -= 1
+        
+        self.ongoing = False   
+    
+    def call_bluff(self, current_player, previous_player):
+        print("Liar!")
+        correct_hands = self.generate_valid_hands(self.playcard, len(self.previous_hand))
+        
+        if self.previous_hand in correct_hands:
+            current_player.roulette()
+            return False # Incorrect bluff call
+        
+        previous_player.roulette()
+        return True # Correct bluff call, previous hand was a bluff!
+            
+    def generate_valid_hands(self, playcard, hand_size):
+        from itertools import product
+        
+        possible_cards = [playcard, 'J']
+        
+        valid_hands = product(possible_cards, hand_size)
+        
+        return [list(hand) for hand in valid_hands]
+        
 class Turn:
-    pass
+    def __init__(self, player):
+        self.player = player
+        self.print_instructions()
+        self.get_action()
+
+    def print_instructions(self):
+        if self.player.instructions == True:
+            print(f"CURRENT PLAYER: {self.player.display_name()}")
+            print("It is now your turn!")
+            print("Enter B to call Bluff.")
+            print("Enter the index 1 - 5 of the cards you want to play separated by just a comma")
+            print("For example if your hand is [K, K, Q, J, Q] you may enter '1,2' to play two cards which are") 
+            print("your first and second cards which both Kings or something like'3' to play your 3rd card (Queen)")
+            print("Enter I to toggle the instruction display.")
+        
+        print("Your hand is currently:" + self.player.hand)
+    
+    def get_action(self):
+        while True:
+            user_input = input("Enter your action: ").upper()
+            
+            if user_input == 'B':
+                return user_input
+                # Call Bluff
+            elif user_input == 'I':
+                self.player.instructions = not self.player.instructions
+                state = "on" if self.player.instructions else "off"
+                print(f"Instructions are now turned {state}.")
+                continue
+            
+            else: 
+                try: 
+                    inputs = [int(i) for i in user_input.split(",")]
+                except ValueError:
+                    print("ERROR: Please enter valid integers separated by commas.")
+                    continue
+                
+                if self.validate_indices(inputs):
+                    play_cards = [self.player.hand[i - 1] for i in inputs]
+                    return play_cards
+                else: 
+                    continue
+    
+    def validate_indices(self, indices):
+        if any(i < 0 or i > len(self.player.hand) for i in indices):
+            print("ERROR: Index out of the range of the player's hand!")
+            return False
+        if len(set(indices) != len(indices)):
+            print("ERROR: Please do not play duplicate indices")
+            return False
+        
+        return True
+            
--- src/player.py
+++ src/player.py
@@ -2,6 +2,21 @@ class Player:
     def __init__(self, name):
         self.name = name
         self.hand = []
-        self.wins = 0
-        self.losses = 0
+        self.alive = True
+        self.max_lives = 6
+        self.instructions = True
+    
+    def eliminate(self):
+        self.alive = False
+        print(self.name + " has been eliminated!")
+        
+    def display_name(self):
+        shots_taken = abs(self.max_lives - 6)
+        return f"{self.name} [{shots_taken}/6]"
+    
+        
+        
+        
+        
+
         
